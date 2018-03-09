d273dc0897dcabad0da3be3f07b358b20d877d73
ycjung <jung3519@gmail.com>
2018-03-09 21:03:14 +0900

    Modify Comment

--- app/KinectTest.cpp
+++ app/KinectTest.cpp
@@ -17,6 +17,8 @@ using namespace telef::cloud;
  * Continuously get frames of point cloud and image.
  *
  * Prints size of pointcloud and size of the image on every frame received
+ * Remove all points that have NaN Position on Receive.
+ * You can check this by watching varying number of pointcloud size
  */
 
 int main(int ac, char* av[]) 
