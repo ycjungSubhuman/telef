408bfeada5d0b9ba0780f2dc28904d06ed63e14d
ycjungSubhuman <ycjung@postech.ac.kr>
2018-06-26 13:08:27 +0900

    Refactor Derivative Test Code to Make it Reusable

--- tests/testutil/diff.h
+++ tests/testutil/diff.h
@@ -1,16 +1,18 @@
 #pragma once
 #include <cstdio>
+#include <cuda_runtime_api.h>
 #include "util/cu_quaternion.h"
 
-//TODO : This only works for specific function and specific input
+typedef void(*func)(float*, const float*);
+
+//TODO : The selection of h only works for specific function and specific input. Make it adaptive.
 __device__
-void calc_numerical_diff(float *result, void (*calc_func)(float*, const float*), int val_dim, int param_dim, const float *x0) {
+void calc_numerical_diff(float *result, func *calc_func,
+                         const float h, int val_dim, int param_dim, const float *x0) {
     float *x1 = (float*)malloc(param_dim*sizeof(float));
     float *x2 = (float*)malloc(param_dim*sizeof(float));
     float *f1 = (float*)malloc(val_dim*sizeof(float));
     float *f2= (float*)malloc(val_dim*sizeof(float));
-
-    const float h = 1e-5;
 
     for (int i=0; i<param_dim; i++) {
         for (int j=0; j<param_dim; j++) {
@@ -31,8 +33,8 @@ void calc_numerical_diff(float *result, void (*calc_func)(float*, const float*),
             }
         }
 
-        calc_func(f2, x2);
-        calc_func(f1, x1);
+        (*calc_func)(f2, x2);
+        (*calc_func)(f1, x1);
 
         for (int j=0; j<val_dim; j++) {
             result[val_dim*i + j] = (double)(f2[j] - f1[j]) / (2.0f * h);
@@ -43,5 +45,63 @@ void calc_numerical_diff(float *result, void (*calc_func)(float*, const float*),
     free(x2);
     free(f1);
     free(f2);
+}
+
+/**
+ * Calculate difference between analytic differentiation and numeric differentiation
+ *
+ * @param diff_d            output matrix. width^param_dim * val_dim*param_dim
+ */
+__global__
+void calc_diff_analytic_numeric(float *diff_d,
+                                func *calc_analytic_diff,
+                                func *calc_function_value,
+                                float initial_value, float delta,
+                                int param_dim, int val_dim,
+                                const int width) {
+    int d = (blockIdx.x * blockDim.x + threadIdx.x);
+    int *inds = (int*)malloc(param_dim*sizeof(int));
+    float *x = (float*)malloc(param_dim*sizeof(float));
+
+    // Setup index and function parameter
+    for(int i=param_dim-1; i>=0; i--) {
+        inds[i] = d % width;
+        d /= width;
+        x[i] = initial_value + inds[i]*delta;
+    }
+
+    float *numeric = (float*)malloc(param_dim*val_dim*sizeof(float));
+    float *analytic = (float*)malloc(param_dim*val_dim*sizeof(float));
+
+    calc_numerical_diff(numeric, calc_function_value, 1e-4, val_dim, param_dim, x);
+    (*calc_analytic_diff)(analytic, x);
+
+    int diff_index=0;
+    for (int i=0; i<param_dim; i++) {
+        diff_index = (diff_index + inds[i])*width;
+    }
+
+    for (int i=0; i<3*3*3; i++) {
+        diff_d[diff_index + i] = analytic[i] - numeric[i];
+    }
+
+    if (diff_index==0) {
+        printf("Analytic(27 samples): ");
+        for (int i=0; i<3*3*3; i++) {
+            printf("%f/", analytic[i]);
+        }
+        printf("\n");
+
+        printf("Numeric(27 samples): ");
+        for (int i=0; i<3*3*3; i++) {
+            printf("%f/", numeric[i]);
+        }
+        printf("\n");
+    }
+
+    free(inds);
+    free(x);
+    free(numeric);
+    free(analytic);
 }
 
--- tests/util/test_cu_quaternion.cu
+++ tests/util/test_cu_quaternion.cu
@@ -1,58 +1,37 @@
 #include <math.h>
 #include <gtest/gtest.h>
 
-#include "util/cu_quaternion.h"
 #include "util/cudautil.h"
 #include "testutil/diff.h"
 
 #define WIDTH 16
-#define BLOCKSIZE 8
+#define BLOCKSIZE 512
 
-__global__
-void calc_diff_analytic_numeric(float *diff_d) {
-    int x = (blockIdx.x * blockDim.x + threadIdx.x);
-    int y = (blockIdx.y * blockDim.y + threadIdx.y);
-    int z = (blockIdx.z * blockDim.z + threadIdx.z);
-    if (!(x < WIDTH && y < WIDTH && z < WIDTH)) return;
-    float u[3] = {
-        -0.08f + 0.01f*x,
-        -0.08f + 0.01f*y,
-        -0.08f + 0.01f*z
-    };
-    float numeric[3*3*3];
-    float analytic[3*3*3];
+__device__ func pcalc_dr_du = calc_dr_du;
+__device__ func pcalc_r_from_u = calc_r_from_u;
 
-    calc_numerical_diff(numeric, &calc_r_from_u, 9, 3, u);
-    calc_dr_du(analytic, u);
-    for (int i=0; i<3*3*3; i++) {
-        diff_d[WIDTH*WIDTH*WIDTH*x + WIDTH*WIDTH*y + WIDTH*z + i] = analytic[i] - numeric[i];
-    }
-    if (x ==4 && y ==4 && z==4) {
-        for (int i=0; i<3*3*3; i++) {
-            printf("%f/", analytic[i]);
-        }
-        printf("\n");
-
-        for (int i=0; i<3*3*3; i++) {
-            printf("%f/", numeric[i]);
-        }
-        printf("\n");
-    }
-}
-
-/**
- * Error btw numerical version and analytic version of dr_du calculation should be less than 1e-1
- *
- * This test is done only around the origin
- */
-TEST(RotationDerivativeTest, AroundZero) {
+void test_dr_dq(float initial, float delta) {
     float *diff = (float*)malloc(3*3*3*WIDTH*WIDTH*WIDTH*sizeof(float));
     float *diff_d;
     CUDA_CHECK(cudaMalloc((void**)(&diff_d), 3*3*3*WIDTH*WIDTH*WIDTH*sizeof(float)));
-    dim3 dimBlock(BLOCKSIZE, BLOCKSIZE, BLOCKSIZE);
-    dim3 dimGrid((WIDTH + BLOCKSIZE-1)/BLOCKSIZE, (WIDTH + BLOCKSIZE-1)/BLOCKSIZE, (WIDTH + BLOCKSIZE-1)/BLOCKSIZE);
-    calc_diff_analytic_numeric<<<dimGrid, dimBlock>>>(diff_d);
+    dim3 dimBlock(BLOCKSIZE);
+    dim3 dimGrid((WIDTH*WIDTH*WIDTH + BLOCKSIZE-1)/BLOCKSIZE);
+
+    // Copy Function Pointers
+    func* calc_dr_du_f = (func*)malloc(sizeof(func));
+    func* calc_r_from_u_f = (func*)malloc(sizeof(func));
+    func *calc_dr_du_d, *calc_r_from_u_d;
+    CUDA_CHECK(cudaMalloc((void**)&calc_dr_du_d, sizeof(func)));
+    CUDA_CHECK(cudaMalloc((void**)&calc_r_from_u_d, sizeof(func)));
+    CUDA_CHECK(cudaMemcpyFromSymbol(calc_dr_du_f, pcalc_dr_du, sizeof(func)));
+    CUDA_CHECK(cudaMemcpyFromSymbol(calc_r_from_u_f, pcalc_r_from_u, sizeof(func)));
+    CUDA_CHECK(cudaMemcpy(calc_dr_du_d, calc_dr_du_f, sizeof(func), cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaMemcpy(calc_r_from_u_d, calc_r_from_u_f, sizeof(func), cudaMemcpyHostToDevice));
+
+    // Run difference calculation
+    calc_diff_analytic_numeric<<<dimGrid, dimBlock>>>(diff_d, calc_dr_du_d, calc_r_from_u_d, initial, delta, 3, 9, WIDTH);
     CHECK_ERROR_MSG("Kernel Error");
+
     CUDA_CHECK(cudaMemcpy((void*)diff, diff_d, 3*3*3*WIDTH*WIDTH*WIDTH*sizeof(float), cudaMemcpyDeviceToHost));
 
     for (int i=0; i<3*3*3*WIDTH*WIDTH*WIDTH; i++) {
@@ -60,11 +39,37 @@ TEST(RotationDerivativeTest, AroundZero) {
     }
 
     //show the 27 values at zero
+    printf("Differences(27 samples): ");
     for (int i=WIDTH*WIDTH*WIDTH; i<WIDTH*WIDTH*WIDTH+3*3*3; i++) {
         printf("%f ", fabsf(diff[i]));
     }
     printf("\n");
 
     CUDA_CHECK(cudaFree(diff_d));
+    CUDA_CHECK(cudaFree(calc_dr_du_d));
+    CUDA_CHECK(cudaFree(calc_r_from_u_d));
     free(diff);
+    free(calc_dr_du_f);
+    free(calc_r_from_u_f);
+}
+
+/**
+ * Error btw numerical version and analytic version of dr_du calculation around (0, 0, 0) should be less than 1e-2
+ */
+TEST(RotationDerivativeTest, AroundZero) {
+    test_dr_dq(-0.08f, 0.01f);
+}
+
+/**
+ * Error btw numerical version and analytic version of dr_du calculation around (1, 1, 1) should be less than 1e-2
+ */
+TEST(RotationDerivativeTest, AroundOne) {
+    test_dr_dq(0.92f, 0.01f);
+}
+
+/**
+ * Error btw numerical version and analytic version of dr_du calculation around (-1, -1, -1) should be less than 1e-2
+ */
+TEST(RotationDerivativeTest, AroundMinusOne) {
+    test_dr_dq(-1.08f, 0.01f);
 }
