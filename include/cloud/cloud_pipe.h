b369839bc1e2e05592c5dea2eee588200122d049
Jake <spacejake@gmail.com>
2018-07-19 17:44:29 +0900

    Add Bilateral Filtering on pointcloud, uses OpenMP impl. Must replace RemoveNaNPoints Pipe for organized point cloud

--- include/cloud/cloud_pipe.h
+++ include/cloud/cloud_pipe.h
@@ -15,4 +15,24 @@ namespace telef::cloud {
     private:
         boost::shared_ptr<DeviceCloudConstT> _processData(boost::shared_ptr<DeviceCloudConstT> in) override;
     };
+
+    /** Remove NaN Positioned Points from PointCloud */
+    class FastBilateralFilterPipe : public Pipe<DeviceCloudConstT, DeviceCloudConstT> {
+    private:
+        using BaseT = Pipe<DeviceCloudConstT, DeviceCloudConstT>;
+        /** standard deviation of the Gaussian used by the bilateral filter for
+         * the spatial neighborhood/window. (size of the Gaussian bilateral filter window to use)
+         */
+        float sigma_s;
+
+        /** standard deviation of the Gaussian used to control how much an adjacent
+         * pixel is downweighted because of the intensity difference (depth in our case).
+         * (standard deviation of the Gaussian for the depth difference)
+         */
+        float sigma_r;
+
+        boost::shared_ptr<DeviceCloudConstT> _processData(boost::shared_ptr<DeviceCloudConstT> in) override;
+    public:
+        FastBilateralFilterPipe(float sigma_s=15, float sigma_r=5e-2);
+    };
 }
--- src/cloud/cloud_pipe.cpp
+++ src/cloud/cloud_pipe.cpp
@@ -1,4 +1,5 @@
 #include <pcl/filters/filter.h>
+#include <pcl/filters/fast_bilateral_omp.h>
 #include <vector>
 
 #include "cloud/cloud_pipe.h"
@@ -19,6 +20,31 @@ namespace telef::cloud {
         result->cloud = boost::make_shared<CloudConstT>(cloudOut);
         result->fx = in->fx;
         result->fy = in->fy;
+        return result;
+    }
+
+
+    FastBilateralFilterPipe::FastBilateralFilterPipe(float sigma_s, float sigma_r)
+            : sigma_s(sigma_s),
+              sigma_r(sigma_r)
+    {}
+
+    boost::shared_ptr<DeviceCloudConstT> FastBilateralFilterPipe::_processData(boost::shared_ptr<DeviceCloudConstT> in) {
+        CloudT cloudOut(*in->cloud);
+        auto cloudIn = in->cloud;
+
+        pcl::FastBilateralFilterOMP<pcl::PointXYZRGBA> filter;
+        filter.setInputCloud(cloudIn);
+        filter.setSigmaS(sigma_s);
+        filter.setSigmaR(sigma_r);
+        filter.applyFilter(cloudOut);
+
+        auto result = boost::make_shared<DeviceCloudT>();
+        result->img2cloudMapping = in->img2cloudMapping;
+        result->cloud = boost::make_shared<CloudConstT>(cloudOut);
+        result->fx = in->fx;
+        result->fy = in->fy;
+
         return result;
     }
 }
