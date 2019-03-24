f4736fda6507dfcd642944e8a36b470c4a4f101b
ycjungSubhuman <ycjung@postech.ac.kr>
2019-03-24 19:42:39 +0900

    Use kinect1 as default

--- 2.run_tracker.sh
+++ 2.run_tracker.sh
@@ -2,7 +2,7 @@
 
 # Run detector
 cd docker/ &&
-    docker build -t telef:kinect2 --build-arg use_kinect_1=0 . &&
+    docker build -t telef:kinect1 --build-arg use_kinect_1=1 . &&
     xhost +local:root; \
     nvidia-docker run -it \
     -v $PWD/../:/workspace \
@@ -10,7 +10,7 @@ cd docker/ &&
     -v /dev/bus/usb:/dev/bus/usb \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    telef:kinect2 /bin/bash -c "cd build/ && \
+    telef:kinect1 /bin/bash -c "cd build/ && \
     cmake ../ && make -j8 && \
     ./PcaTargetFit -V \
     -M ../pcamodels/bs-5 \
